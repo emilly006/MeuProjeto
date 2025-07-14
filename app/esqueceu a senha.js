d4c29f801a45919590d36d9d5e2b9230063eb2c6
Emilly gabriely <egtm1@aluno.ifal.edu.br>
2025-07-14 11:30:25 -0300

    criação das telas fakes

---
+++ app/esqueceu a senha.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+
+const LotsOfStyles = () => {
+  return (
+    <View style={styles.container}>
+          <Text style={styles.title}>Tela Esqueceu a senha</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  }
+})
+export default LotsOfStyles;
+
---
+++ app/telaDeJogos.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+
+const LotsOfStyles = () => {
+  return (
+    <View style={styles.container}>
+          <Text style={styles.title}>Tela de jogos</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#9C1E94',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  }
+})
+export default LotsOfStyles;
+
